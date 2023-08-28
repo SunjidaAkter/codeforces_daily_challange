8bf8df768bc0272f198227c418dc0933086d1ef8
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-28 06:40:53 +0600

    mission accomplished 🔥🚀🔥

--- E_Cardboard_for_Pictures.cpp
+++ E_Cardboard_for_Pictures.cpp
@@ -31,38 +31,5 @@ int main(){
     }
     return 0;
 }
-// ll n, c, x;
-// vector<ll> a;
- 
-// ll check(ll mid) {
-//     ll sum = 0;
- 
-//     for (int i : a) {
-//         sum += (i + 2 * mid) * (i + 2 * mid);
-//         if (sum > c) return 1;
-//     }
- 
-//     if (sum < c) return -1;
-//     return 0;
-// }
- 
-// int main() {
-//     int t; cin >> t;
- 
-//     while (t--) {
-//         cin >> n >> c;
-//         a.clear();
- 
-//         for (int i = 0; i < n; i++) {cin >> x; a.push_back(x);}
- 
-//         ll l = 0, r = 1e9, mid;
-//         while (l <= r) {
-//             mid = (l + r)/2;
-//             if (check(mid) == 1) r = mid - 1;
-//             else if (check(mid) == -1) l = mid + 1;
-//             else break;
-//         }
- 
-//         cout << mid << endl;
-//     }
-// }
+//* what i learnt
+//* akta range er middle value k probable value dhore ota ke perfect korar jonno while loop chaliechhi;
