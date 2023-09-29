25fafecfe70b07d791b7605e6fab2b1909f97138
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-09-29 09:53:47 +0600

    mission accomplished 🔥🚀🔥

---
+++ C_Non_coprime_Split.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int>pii;
+#define ll long long
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int l,r;
+        cin>>l>>r;
+        int a=1;
+        cout<<1<<" "<<l;
+
+    }
+    return 0;
+}
