7d9fe25e5182bc56bc93dff894d000b35a562c92
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-10-05 11:20:58 +0600

    mission accomplished 🔥🚀🔥

--- A_Lucky_Division.cpp
+++ A_Lucky_Division.cpp
@@ -3,21 +3,24 @@ using namespace std;
 typedef pair<int,int>pii;
 #define forl(var,str,end) for(long long int var=str; var<end; var++)
 #define ll long long
+bool lucky(int x){
+
+}
 int main(){
-    int n,sum=0,s1=0;cin>>n;
+    int p;cin>>p;
+    int n=p;
     bool flag=true;
     while(n!=0){
         if(n%10!=7&&n%10!=4){
             // cout<<n%10<<"K";
-            
-            sum++;
+            flag=false;
+            break;
         }
         n/=10;
-        s1++;
     }
-    if(sum==s1)cout<<"YES";
+    if(flag)cout<<"YES";
     else {
-        if(n%4==0||n%7==0){
+        if(p%4==0||p%47==0||p%744==0||p%7==0){
             cout<<"YES";
         }else cout<<"NO";    
     }
---
+++ A_green_gold_dog_array_and_permutation.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pub push_back
+#define pob pop_back
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define y cout << "Yes" << endl
+#define n cout << "No" << endl
+typedef pair<ll,ll>pii;
+#define forl(var,str,end) for(long long int var=str; var<end; var++)
+void solve(){
+    yes;
+}
+int main(){
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
